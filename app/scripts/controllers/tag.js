69eb1ed4b638c755f2cbf5f5dc475b1a0c5f735f
emas <emascarinas@gmail.com>
2015-04-18 17:09:06 -0400

    dsf

--- app/scripts/controllers/tag.js
+++ app/scripts/controllers/tag.js
@@ -1,9 +1,13 @@
 'use strict';
 angular.module('epicoverflowApp')
         .controller('TagCtrl', function($scope, user, config, util, $timeout) {
+            
             $scope.currentPage = 1;
             $scope.maxSize = config.pageMaxSize;
             $scope.itemsPerPage = config.itemsPerPage;
+            $scope.boundaryLinks = true;
+            $scope.rotate = false;
+            
             $scope.fetch = function() {
                 $timeout(function() {
                     fetch();
